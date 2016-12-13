4a19798b6c515dced061efcb17431bd8a4a75275
Vojtěch Kijenský <vojtech@socifi.com>
2016-12-13 14:51:11 +0100

    Synchronous endpoint /report is deprecated. This fix using asynchronous way with queue support.

--- src/Wrappers/Report.php
+++ src/Wrappers/Report.php
@@ -23,10 +23,9 @@ use Positivezero\Rest;
  * @see \Positivezero\Adzerk\Wrapper;
  * @author Vojtěch Kijenský <vojtech@positivezero.co.uk>
  *
-
  * You can specify the criteria for the report using an object titled "criteria".
- * @property string StartDate -  m/d/Y - The start date for the report
- * @property string EndDate -  m/d/Y - The end date for the report
+ * @property string StartDate -  Y-m-dTH:i:s - The start date for the report
+ * @property string EndDate -  Y-m-dTH:i:s - The end date for the report
  * @property array GroupBy - List of strings - The values to group the report on. The possible values here are day, week, month, brandId, campaignId, optionId, creativeId, adTypeId, siteId, zoneId, countryCode, metroCode, keyword.
  * @property boolean Top30Countries -  If set to true only returns data from the top 30 countries as calculated by total network traffic.
  * @property boolean Exclude3rdParty - If set to true will exclude HTML/Javascript ads from the report.
@@ -35,28 +34,60 @@ use Positivezero\Rest;
  */
 class Report extends Wrapper
 {
+    /** @var string UUID of queued report */
+    private $uuid;
 
-	/**
-	 *
-	 * (Note: flightId will be returned as OptionId in the report.)
-	 * Here is a sample criteria object that will generate a daily report for the Advertiser with an id of 12 grouped by campaign:
-	 * criteria={'StartDate': '1/1/2011', 'EndDate': '5/16/2011', 'GroupBy': ['day','campaignId'], 'Parameters': [{'brandId': 12}]}
-	 *
-	 * @return mixed
-	 * @throws RequestException
-	 */
-	public function get()
-	{
-		try {
-			$response = $this->request->post($this->getRestMethod(), (string)$this);
-			return $response->decoded_response;
-		} catch (Rest\RestClientException $e) {
-			throw new RequestException($e->getMessage(),null,$e);
-		}
-	}
+    /**
+     *
+     * Request will return Id that identify request in the queue. Result must be obtained later via get() method with the Id.
+     * Here is a sample criteria object that will generate a daily report for the Advertiser with an id of 12 grouped by campaign:
+     * criteria={'StartDate':'2016-12-13T00:00:00','EndDate':'2016-12-13T23:59:59', 'GroupBy': ['day','campaignId'], 'Parameters': [{'brandId': 12}]}
+     *
+     * @return mixed
+     * @throws RequestException
+     */
+    public function post()
+    {
+        try {
+            $response = $this->request->post(
+                'report/queue',
+                (string) $this
+            );
+            return $response->decoded_response;
+        } catch (Rest\RestClientException $e) {
+            throw new RequestException($e->getMessage(), null, $e);
+        }
+    }
 
-	public function __toString()
-	{
-		return 'criteria=' . json_encode($this->data);
-	}
+    public function __toString()
+    {
+        return 'criteria=' . json_encode($this->data);
+    }
+
+    /**
+     *
+     * (Note: flightId will be returned as OptionId in the report.)
+     * Here is a sample criteria object that will generate a daily report for the Advertiser with an id of 12 grouped by campaign:
+     * criteria={'StartDate':'2016-12-13T00:00:00','EndDate':'2016-12-13T23:59:59', 'GroupBy': ['day','campaignId'], 'Parameters': [{'brandId': 12}]}
+     *
+     * @return mixed
+     * @throws RequestException
+     */
+    public function get()
+    {
+        try {
+            $response = $this->request->get('report/queue/' . $this->uuid);
+            return $response->decoded_response;
+        } catch (Rest\RestClientException $e) {
+            throw new RequestException($e->getMessage(), null, $e);
+        }
+    }
+
+    /**
+     * @param string $uuid
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+    }
 }
